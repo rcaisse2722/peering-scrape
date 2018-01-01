2aa7cc9846469636fefb56f354f853d9ac0e88fe
Ross Caisse <rcaisse@gmail.com>
2017-12-31 22:23:49 -0500

    Parses out everything correctly and prints to CSV file

--- peering_entry.py
+++ peering_entry.py
@@ -2,12 +2,19 @@
 
 from datetime import datetime
 
-
 class PeeringEntry:
     timestamp = datetime.min
+
+    def __init__(self, participant, timestamp):
+        self.timestamp = timestamp
+        self.participant = participant
+
+class Participant:
     name = ''
     id = ''
+    url = ''
 
-    def __init__(self, name, id):
+    def __init__(self, name, id, url):
         self.name = name
         self.id = id
+        self.url = url
--- peering_scrape.py
+++ peering_scrape.py
@@ -3,6 +3,47 @@
 import urllib2
 from bs4 import BeautifulSoup
 from peering_entry import PeeringEntry
+from peering_entry import Participant
+from datetime import datetime, timedelta
+
+''' 
+    Parse HTML tag that represents participant
+    Args: tag
+    Return: participant object
+'''
+def parseParticipant(tag):
+
+    if tag is None:
+        raise ValueError("Participant tag is NULL")
+
+    name = tag.find("a").string
+    url = "{}{}".format(scrape_page, tag.find("a")["href"])
+    id = tag.contents[2]
+    id = id[id.find("(") + 1:id.find(")")]
+    return Participant(name, id, url)
+
+
+''' 
+    Parse HTML tag that represents age
+    Args: tag
+    Return: timestamp
+'''
+def parseAge(tag):
+    if tag is None:
+        raise ValueError("Age tag is NULL")
+
+    now = datetime.now()
+    delta = 0
+
+    if "minutes" in tag.string:
+        delta = int(tag.string.split("minutes")[0])
+    elif "hours" in tag.string:
+        delta = int(tag.string.split("hours")[0]) * 60
+    else:
+        raise ValueError("Failed to parse age: {}".format(tag.string))
+
+    return now - timedelta(minutes=delta)
+
 
 print 'Starting script...'
 
@@ -31,12 +72,18 @@ ages = parent_tag.find_all("div", { "class" : "age" })
 if len(participants) != len(ages):
     print "Length of participants {} does not match length of ages {}".format(len(participants), len(ages))
 
+entries = [] # list of PeeringEntry
+
 with open('output.csv', 'w') as outputfile:
     for i in range(0, len(participants)):
-        outputfile.writelines("{},{}".format(participants[i], ages[i]))
 
+        # TODO Add error checking for index out bounds, etc
+        #name = participants[i].find("a").string
+        #url = "{}{}".format(scrape_page, participants[i].find("a")["href"])
+        #id = participants[i].contents[2]
+        #id = id[id.find("(") + 1:id.find(")")]
+        entry = PeeringEntry(parseParticipant(participants[i]), parseAge(ages[i]))
+        entries.append(entry)
 
-# iterate through children
-# children = parent_tag.findChildren()
-#for child in children:
-#    print child
+        outputfile.writelines("{},{},{},{}\n".format(
+            entry.participant.name, entry.participant.id, entry.participant.url, entry.timestamp))
