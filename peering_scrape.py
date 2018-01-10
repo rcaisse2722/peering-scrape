10b3a1966824516804b1954939ad8a3f50556c93
Ross Caisse <rcaisse@gmail.com>
2018-01-09 20:34:47 -0500

    Making some additional changes

--- peering_scrape.py
+++ peering_scrape.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import urllib2
+import os.path
+import time
 from bs4 import BeautifulSoup
 from peering_entry import PeeringEntry
 from peering_entry import Participant
@@ -33,7 +35,6 @@ def parseAge(tag):
         raise ValueError("Age tag is NULL")
 
     now = datetime.now()
-    delta = 0
 
     if "minutes" in tag.string:
         delta = int(tag.string.split("minutes")[0])
@@ -45,45 +46,55 @@ def parseAge(tag):
     return now - timedelta(minutes=delta)
 
 
+def getPageData():
+    # query the website and return the html to the variable page
+    # UNCOMMENT TO GRAB ACTUAL DATA
+    # return urllib2.urlopen(scrape_page)
+
+    with open('sample_data.html', 'r') as testFile:
+        return testFile.read();
+
 print 'Starting script...'
 
 # page to scrape
 scrape_page = 'https://www.peeringdb.com'
 
 # number of seconds in 15 minutes...
-interval = 900
+interval = 30
 
-# query the website and return the html to the variable page
-# UNCOMMENT TO GRAB ACTUAL DATA
-# page = urllib2.urlopen(scrape_page)
+# list of PeeringEntry
+entries = []
 
-with open('sample_data.html', 'r') as testFile:
-    page = testFile.read();
+# read in file if it exists
+if os.path.isfile("output.csv"):
 
-# parse it out...
-parsed_data = BeautifulSoup(page, 'html.parser')
 
-# get the div that encompasses data we care about
-parent_tag = parsed_data.find('h4', text='NETWORKS').parent
 
-participants = parent_tag.find_all("div", { "class" : "participant"})
-ages = parent_tag.find_all("div", { "class" : "age" })
+while 1:
 
-if len(participants) != len(ages):
-    print "Length of participants {} does not match length of ages {}".format(len(participants), len(ages))
+    page = getPageData()
 
-entries = [] # list of PeeringEntry
+    # parse it out...
+    parsed_data = BeautifulSoup(page, 'html.parser')
 
-with open('output.csv', 'w') as outputfile:
-    for i in range(0, len(participants)):
+    # get the div that encompasses data we care about
+    parent_tag = parsed_data.find('h4', text='NETWORKS').parent
 
-        # TODO Add error checking for index out bounds, etc
-        #name = participants[i].find("a").string
-        #url = "{}{}".format(scrape_page, participants[i].find("a")["href"])
-        #id = participants[i].contents[2]
-        #id = id[id.find("(") + 1:id.find(")")]
-        entry = PeeringEntry(parseParticipant(participants[i]), parseAge(ages[i]))
-        entries.append(entry)
+    participants = parent_tag.find_all("div", { "class" : "participant"})
+    ages = parent_tag.find_all("div", { "class" : "age" })
 
-        outputfile.writelines("{},{},{},{}\n".format(
-            entry.participant.name, entry.participant.id, entry.participant.url, entry.timestamp))
+
+    if len(participants) != len(ages):
+        raise ValueError("Length of participants {} does not match length of ages {}".format(
+            len(participants), len(ages)))
+
+    # output
+    with open('output.csv', 'w') as outputfile:
+        for i in range(0, len(participants)):
+            entry = PeeringEntry(parseParticipant(participants[i]), parseAge(ages[i]))
+            entries.append(entry)
+
+            outputfile.writelines("{},{},{},{}\n".format(
+                entry.participant.name, entry.participant.id, entry.participant.url, entry.timestamp))
+
+    time.sleep(interval)
